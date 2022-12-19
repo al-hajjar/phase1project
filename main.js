c22c737f9bbcd17ebf0f1f7236451901bb650ad9
kidhri salim <salimkhidhri@gmail.com>
2022-12-20 02:23:23 +0300

    jsonlink

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-// main.js
-
-const form = document.querySelector('form');
-const submitButton = form.querySelector('button[type="submit"]');
-
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  console.log('Form submitted!');
-});
+function gettingCats(cats){
+    fetch("https://al-hajjar.github.io/phase1project/db.json")
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+}
