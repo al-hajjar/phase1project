9ce1d76069d736e3a5cf29df2cf31361384386a8
kidhri salim <salimkhidhri@gmail.com>
2022-12-20 17:25:40 +0300

    designed card to display cats

--- main.js
+++ main.js
@@ -1,7 +1,60 @@
-function gettingCats(cats){
-    fetch("https://al-hajjar.github.io/phase1project/db.json")
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
+const CATEGORIES = 'https://al-hajjar.github.io/phase1project/db.json'
+document.addEventListener('DOMContentLoaded', () => {
+    // ROWS DATA
+    const mealCategoryRow = document.getElementById('meal-categories')
+    const hide = document.querySelector(".Landing-page")
+    // LINKS DATA
+    const categoriesLink = document.getElementById('category-link')
+    const homeLink = document.getElementById('home-link')
+    // CLICK EVENTS FOR LINKS
+    categoriesLink.addEventListener('click', () => {
+        // hide.style.display = "none"
+        // show categories
+        mealCategoryRow.removeAttribute('hidden')
+        mealCategoryRow.style.display = "inline-block"
     })
-}
+      // create category element
+      const createCategory = (image, breed, habitat, lifespan) => {
+        const rootDiv = document.createElement('div')
+        rootDiv.classList.add('col-4', 'p-1')
+
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('card', 'col-12', 'p-2')
+
+        const categoryImg = document.createElement('img')
+        categoryImg.classList.add('card-img-top')
+        categoryImg.src = image
+
+        const categoryTitle = document.createElement('h4')
+        categoryTitle.classList.add('card-title')
+        categoryTitle.innerText = breed
+
+        const categoryHabitat = document.createElement('p')
+        categoryHabitat.classList.add('card-text')
+        categoryHabitat.innerText = "Habitat: " + habitat
+
+        const categoryLifeSpan = document.createElement('p')
+        categoryLifeSpan.classList.add('card-text')
+        categoryLifeSpan.innerText = "Life Span: " + lifespan
+        // append title and image to card
+        cardDiv.appendChild(categoryImg)
+        cardDiv.appendChild(categoryTitle)
+        cardDiv.appendChild(categoryHabitat)
+        cardDiv.appendChild(categoryLifeSpan)
+        rootDiv.appendChild(cardDiv)
+        return rootDiv
+    }
+    const loadCategories = () => {
+        fetch(CATEGORIES)
+            .then((response) => response.json())
+            .then((data) => {
+                const categoriesData = data.cats
+                const categoryElems = categoriesData.map(
+                    cats => createCategory(cats.image, cats.breed, cats.habitat, cats.lifespan)
+                )
+                mealCategoryRow.append(...categoryElems)
+            })
+    }
+    loadCategories()
+})
+
